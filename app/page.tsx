0528bd992fb7fda6c5e48934c9f1f7027684e689
KFuczek <krzy96@interia.pl>
2023-07-21 11:03:42 +0200

    fix/ render only on client side

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,18 @@
 import './globals.css';
 import styles from './page.module.scss';
 import MainFullScreen from '../src/components/mainFullScreen/page';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
-  return (
+  const [isSSR, setIsSSR] = useState(true);
+  useEffect(() => {
+    setIsSSR(false);
+  }, []);
+
+  return isSSR ? (
     <div className={styles.frontPage}>
       <MainFullScreen />
       <div className={styles.container}>container</div>
     </div>
-  );
+  ) : null;
 }
