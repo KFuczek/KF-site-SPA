9f42581fef2f407a284f190b25b48afb238ce727
KFuczek <krzy96@interia.pl>
2023-08-11 10:47:38 +0200

    feat/ modify banners

--- src/components/mainFullScreen/scyText/page.tsx
+++ src/components/mainFullScreen/scyText/page.tsx
@@ -3,26 +3,37 @@ import styles from './page.module.scss';
 export default function Sun() {
   return (
     <div className={styles.textContainer}>
-      <div className={styles.triangle} />
-      <div className={styles.text}>
-        <span className={styles.line1}>
-          <span className={styles.letter}>n</span>
-          <span className={styles.letter}>e</span>
-          <span className={styles.letter}>w</span>
-        </span>
-        <span className={styles.line2}>
-          <span className={styles.letter}>R</span>
-          <span className={styles.letter}>e</span>
-          <span className={styles.letter}>t</span>
-          <span className={styles.letter}>r</span>
-          <span className={styles.letter}>o</span>
-        </span>
-        <span className={styles.line3}>
-          <span className={styles.letter}>w</span>
-          <span className={styles.letter}>a</span>
-          <span className={styles.letter}>v</span>
-          <span className={styles.letter}>e</span>
-        </span>
+      <div className={styles.textWrapper}>
+        <div className={styles.triangle} />
+        <div className={styles.text}>
+          <span className={styles.line1}>
+            <span className={styles.letter}>s</span>
+            <span className={styles.letter}>c</span>
+            <span className={styles.letter}>r</span>
+            <span className={styles.letter}>o</span>
+            <span className={styles.letter}>l</span>
+            <span className={styles.letter}>l</span>
+          </span>
+          <span className={styles.line2}>
+            <span className={styles.letter}>t</span>
+            <span className={styles.letter}>h</span>
+            <span className={styles.letter}>r</span>
+            <span className={styles.letter}>o</span>
+            <span className={styles.letter}>u</span>
+            <span className={styles.letter}>g</span>
+            <span className={styles.letter}>h</span>
+          </span>
+          <span className={styles.line3}>
+            <span className={styles.letter}>f</span>
+            <span className={styles.letter}>o</span>
+            <span className={styles.letter}>r</span>
+            <span className={styles.letter}> </span>
+            <span className={styles.letter}>m</span>
+            <span className={styles.letter}>o</span>
+            <span className={styles.letter}>r</span>
+            <span className={styles.letter}>e</span>
+          </span>
+        </div>
       </div>
     </div>
   );
